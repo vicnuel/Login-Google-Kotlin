5a75563c6727890a9548785a1f4ee0e397bbe5af
Victor Emanuel <78376383+vicnuel@users.noreply.github.com>
2024-08-04 12:41:35 -0300

    add layout

---
+++ app/src/main/java/com/vicnuel/login/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.vicnuel.login
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.vicnuel.login.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initButtonGoogle()
+    }
+
+    private fun initButtonGoogle() {
+        val textButtonGoogle = binding.buttonGoogle.getChildAt(0) as TextView
+        textButtonGoogle.text = getString(R.string.entrar_com_google)
+    }
+}
